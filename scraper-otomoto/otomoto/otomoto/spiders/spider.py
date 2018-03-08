41b89673d37330bd0c8c53dff32831ec06349da7
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2018-03-08 14:29:35 +0100

    Extract some params from actual otomoto offers

--- scraper-otomoto/otomoto/otomoto/spiders/spider.py
+++ scraper-otomoto/otomoto/otomoto/spiders/spider.py
@@ -4,6 +4,10 @@ import scrapy
 class OtomotoSpider(scrapy.Spider):
     name = "otomoto"
 
+    offers_xpath = '//*[@id="body-container"]//h2[@class="offer-title"]/a/@href'
+    price_xpath = '//*[@id="siteWrap"]//span[@class="offer-price__number"]/text()'
+    car_params_xpath = '//*[@id="parameters"]/ul[1]/li[span="Kategoria"]/div/a/@title'
+
     def start_requests(self):
         base_url = 'https://www.otomoto.pl/osobowe/?page='
 
@@ -13,11 +17,13 @@ class OtomotoSpider(scrapy.Spider):
 
     def parse(self, response):
         selector = scrapy.Selector(response)
-        offers = selector.xpath('//*[@id="body-container"]//h2[@class="offer-title"]/a/@href')
+        offers = selector.xpath(self.offers_xpath)
         offers_urls = offers.extract()
 
         for url in offers_urls:
             yield scrapy.Request(url, callback=self.parse_offer, meta={'url': url})
 
     def parse_offer(self, response):
-        pass
+        selector = scrapy.Selector(response)
+        price = int(selector.xpath(self.price_xpath).extract_first().replace(' ', ''))
+        category = selector.xpath(self.car_params_xpath).extract_first()
