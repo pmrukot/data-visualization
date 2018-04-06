f35fa2bcc391e1e4bdc472335f22bd18fea51720
Maciej Rapacz <maciek.rapacz@gmail.com>
2018-04-06 12:22:41 +0200

    Add script

---
+++ graphloader.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['reddit']
+
+subs = db.subreddits.find()
+nodes = [sub['_id'] for sub in subs]
+
+relations = db.relations.find()
+edges = [(r['sub_a'], r['sub_b']) for r in relations]
+
+s_nodes = nodes[:1000]
+s_edges = [edge for edge in edges if edge[0] in s_nodes and edge[1] in s_nodes]
+
