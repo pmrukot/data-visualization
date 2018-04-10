25539f2e3ef34fdf8dec938a26462d1c1670495a
Maciej Rapacz <maciek.rapacz@gmail.com>
2018-04-10 21:18:31 +0200

    Add gephi visualiations

--- subreddits/graphloader.py
+++ subreddits/graphloader.py
@@ -4,11 +4,11 @@ client = MongoClient()
 db = client['reddit']
 
 subs = db.subreddits.find()
-nodes = [sub['_id'] for sub in subs]
+nodes = [(sub['_id'],{'type': sub['type']}) for sub in subs]
 
 relations = db.relations.find()
 edges = [(r['sub_a'], r['sub_b']) for r in relations]
 
-s_nodes = nodes[:1000]
+s_nodes = nodes[:10000]
 s_edges = [edge for edge in edges if edge[0] in s_nodes and edge[1] in s_nodes]
 
